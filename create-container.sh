f095b8a9590845347a40ddf4c46a13c3ef8267ae
Bjarki Gunnarsson <bjg60@hi.is>
2020-06-08 15:02:39 +0000

    make control flow clearer

--- create-container.sh
+++ create-container.sh
@@ -1,34 +1,47 @@
 #!/usr/bin/bash -x
 
-ETHDEV=$(route | grep '^default' | grep -o '[^ ]*$')
-
 create_container() {
     local name=$1
     local dest_port=$2
+    local withpasswd=$3
+    
     local ct_path="/var/lib/lxc/ct-$name/rootfs"
+    local ETHDEV=$(route | grep '^default' | grep -o '[^ ]*$')
 
     exec <dist
+
+    cp /etc/lxc/default.conf ~/.config/lxc/default.conf
+    echo "lxc.idmap = u 0 200000 65536" >> /root/.config/lxc/default.conf
+    echo "lxc.idmap = g 0 200000 65536" >> /root/.config/lxc/default.conf
 
     lxc-create -t download -n ct-$name
     lxc-start -n ct-$name
     sleep 4 # let it fully start
 
-    lxc-attach -n ct-$name -- adduser --disabled-password --gecos ",,,," $name
+    if $withpasswd; then
+        lxc-attach -n ct-$name -- adduser --gecos ",,,," $name
+    else   
+        lxc-attach -n ct-$name -- adduser --disabled-password --gecos ",,,," $name
+    fi
+
     lxc-attach -n ct-$name -- adduser $name sudo
     lxc-attach -n ct-$name -- apt install openssh-server
 
-    IP=$(lxc-info -n ct-$name -iH)
+    local IP=$(lxc-info -n ct-$name -iH)
 
     iptables -t nat -A PREROUTING -i $ETHDEV -p tcp --dport $dest_port -j DNAT --to $IP:22
     iptables-save > /etc/iptables/rules.v4
 
     echo "lxc.start.auto = 1" >> /var/lib/lxc/ct-$name/config
 
-    if $3; then
+    if [ ! $withpasswd ]; then
 	    mkdir $ct_path/home/$name/.ssh 
         cp authorized_keys $ct_path/home/$name/.ssh/
         lxc-attach -n ct-$name -- chown $name -R /home/$name/.ssh
         lxc-attach -n ct-$name -- chgrp $name -R /home/$name/.ssh
     fi
 }
+
+lxc-destroy -fn ct-laddi
+create_container laddi 2208 false
 
