36010dcd13e4e7af82b92caab135bdc70f7223e6
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2020-06-08 16:13:17 +0000

    Change method of supplying authorized_keys file

--- create-container.sh
+++ create-container.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/bash -x
+#!/usr/bin/env bash
+
+set -x
 
 create_container() {
     local name=$1
@@ -14,31 +16,31 @@ create_container() {
     echo "lxc.idmap = u 0 200000 65536" >> /root/.config/lxc/default.conf
     echo "lxc.idmap = g 0 200000 65536" >> /root/.config/lxc/default.conf
 
-    lxc-create -t download -n ct-$name
-    lxc-start -n ct-$name
+    lxc-create -n ct-"$name" -t download -- --no-validate
+    lxc-start -n ct-"$name"
     sleep 4 # let it fully start
 
     if $withpasswd; then
-        lxc-attach -n ct-$name -- adduser --gecos ",,,," $name
-    else   
-        lxc-attach -n ct-$name -- adduser --disabled-password --gecos ",,,," $name
+        lxc-attach -n ct-"$name" -- adduser --gecos ",,,," "$name"
+    else
+        lxc-attach -n ct-"$name" -- adduser --disabled-password --gecos ",,,," "$name"
     fi
 
-    lxc-attach -n ct-$name -- adduser $name sudo
-    lxc-attach -n ct-$name -- apt install openssh-server
+    lxc-attach -n ct-"$name" -- adduser "$name" sudo
+    lxc-attach -n ct-"$name" -- apt install openssh-server
 
-    local IP=$(lxc-info -n ct-$name -iH)
+    local IP=$(lxc-info -n ct-"$name" -iH)
 
-    iptables -t nat -A PREROUTING -i $ETHDEV -p tcp --dport $dest_port -j DNAT --to $IP:22
+    iptables -t nat -A PREROUTING -i "$ETHDEV" -p tcp --dport "$dest_port" -j DNAT --to "$IP":22
     iptables-save > /etc/iptables/rules.v4
 
-    echo "lxc.start.auto = 1" >> /var/lib/lxc/ct-$name/config
+    echo "lxc.start.auto = 1" >> /var/lib/lxc/ct-"$name"/config
 
     if [ ! $withpasswd ]; then
-	    mkdir $ct_path/home/$name/.ssh 
-        cp authorized_keys $ct_path/home/$name/.ssh/
-        lxc-attach -n ct-$name -- chown $name -R /home/$name/.ssh
-        lxc-attach -n ct-$name -- chgrp $name -R /home/$name/.ssh
+        lxc-attach -n ct-"$name" -- mkdir /home/"$name"/.ssh
+        cat authorized_keys | lxc-attach -n ct-"$name" \
+            -- /bin/sh -c "/bin/cat > /home/$name/.ssh/authorized_keys"
+        lxc-attach -n ct-"$name" -- chown "$name:$name" -R /home/"$name"/.ssh
     fi
 }
 
