49f71bc2e159f5644b938bf663c94c048c353b0b
Bjarki Gunnarsson <bjg60@hi.is>
2020-06-15 14:21:02 +0000

    automatically make parent directories and fix minor errors

--- create-container.sh
+++ create-container.sh
@@ -7,12 +7,13 @@ create_container() {
     local dest_port="$2"
     local opt="$3"    # only takes "withsk": with shared key
 
-    local ETHDEV
-    ETHDEV=$(route | grep '^default' | grep -o '[^ ]*$')
+    local ETHDEV=$(route | grep '^default' | grep -o '[^ ]*$')
 
+    mkdir -p /root/.config/lxc
     cp /etc/lxc/default.conf /root/.config/lxc/default.conf
-    echo "lxc.idmap = u 0 200000 65536" >> /root/.config/lxc/default.conf
-    echo "lxc.idmap = g 0 200000 65536" >> /root/.config/lxc/default.conf
+    # the containers act as if they are the first normal user on the host
+    echo "lxc.idmap = u 0 100000 65536" >> /root/.config/lxc/default.conf
+    echo "lxc.idmap = g 0 100000 65536" >> /root/.config/lxc/default.conf
 
     lxc-create -n ct-"$name" -t download -- --no-validate -d ubuntu -r bionic -a amd64
     lxc-start -n ct-"$name"
@@ -27,9 +28,9 @@ create_container() {
     lxc-attach -n ct-"$name" -- adduser "$name" sudo
     lxc-attach -n ct-"$name" -- apt install openssh-server
 
-    local IP
-    IP=$(lxc-info -n ct-"$name" -iH)
+    local IP=$(lxc-info -n ct-"$name" -iH)
 
+    mkdir -p /etc/iptables
     iptables -t nat -A PREROUTING -i "$ETHDEV" -p tcp --dport "$dest_port" -j DNAT --to "$IP":22
     iptables-save > /etc/iptables/rules.v4
 
@@ -43,7 +44,7 @@ create_container() {
     fi
 }
 
-if [ "$#" = 0 ]; then
+if [ "$#" != 3 ]; then
     exit
 else
     create_container "$@"
--- create-shared.sh
+++ create-shared.sh
@@ -5,6 +5,6 @@ set -x
 # get the create_container function
 . create-container.sh
 
-create_container miner 412 true
-create_container devel 404 true
+create_container miner 412 withsk
+create_container devel 415 withsk
 
